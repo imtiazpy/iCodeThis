30aafbfd23ba9c817b58b092b4afce16b7f6f40b
imtiazpy <ahmed.imti1994@gmail.com>
2022-12-28 19:19:23 +0600

    working on the fancy slider challenge

---
+++ fancy-sliders/index.js
@@ -0,0 +1,14 @@
+// arrow-range
+const vertical_bar_size_arr = [24, 40, 38, 44, 46, 50, 45, 40, 38, 38]
+
+const vertical_bar_container = document.querySelector('.vertical-bars');
+
+for (let index = 0; index < 5; index++) {
+    vertical_bar_size_arr.forEach((size) => {
+        const element = document.createElement('div');
+        element.classList.add('vertical-bar');
+        element.style.height = size + 'px';
+
+        vertical_bar_container.appendChild(element)
+    })
+}
